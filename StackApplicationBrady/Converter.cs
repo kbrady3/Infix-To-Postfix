c9c3eb65900169121c3be21414f6d082b15eb34e
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-02-11 18:22:23 -0600

    Initialization

---
+++ StackApplicationBrady/Converter.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackApplicationBrady
+{
+    public class Converter
+    {
+        public string Convert(string infix)
+        {
+            Stack stack = new Stack();
+
+            string output = "";
+
+            foreach (char c in infix)
+            {
+                if (Char.IsNumber(c))
+                {
+                    output += c;
+                }
+                else if (Char.IsLetter(c))
+                {
+                    output += c;
+                }
+                else if (c == '(' || c == ')' || c == '+' || c == '-' || c == '*' || c == '/' || c == '^')
+                {
+                    if(stack.Count > 0)
+                    {
+                        bool allElementsSorted = false;
+
+                        while (!allElementsSorted)
+                        {
+                            int topOfStackPrecedence = 0;
+
+                            if (stack.Count > 0)
+                            {
+                                topOfStackPrecedence = Precedence(stack.Peek().ToString()); //Gets the precedence of the top item in stack
+                            }
+                            else
+                            {
+                                break;
+                            }
+                            
+                            int currPrecedence = Precedence(c.ToString()); //Gets precedence of current item
+                            if (topOfStackPrecedence < currPrecedence)
+                            {
+                                stack.Push(c); //Push current symbol
+                            }
+                            else
+                            {
+                                stack.Pop();
+                                stack.Push(c);
+                            }
+
+                            if (c == ')')
+                            {
+                                string top = "";
+                                
+                                if(stack.Count > 0)
+                                {
+                                    top = stack.Peek().ToString();
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                                
+                                while(top != "(")
+                                {
+                                    
+                                    output += top; //Write operators to output
+                                    stack.Pop(); //Then pop them from the stack
+                                }
+
+                                stack.Pop(); //Pops the left parenthesis
+                            }
+
+                            if(stack.Count <= 0)
+                            {
+                                allElementsSorted = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        stack.Push(c); //Pushes element onto stack if stack is null
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        public int Precedence(string symbol)
+        {
+            if (symbol == "(" || symbol == ")")
+            {
+                return 4;
+            }
+            if (symbol == "^")
+            {
+                return 3;
+            }
+            else if (symbol == "*" || symbol == "/")
+            {
+                return 2;
+            }
+            else if (symbol == "+" || symbol == "-")
+            {
+                return 1;
+            }
+            else
+            {
+                return (0);
+            }
+        }
+
+//                    //Places all operators in the stack, in order
+//            foreach(char c in infix)
+//            {
+//                if (c == '(' || c == ')' || c == '+' || c == '-' || c == '*' || c == '/' || c == '^')
+//                {
+//                    numOfOperators++;
+//                    int currPrecedence = Precedence(c);
+
+//                    //Separates operators out into 3 strings based on precedence
+//                    switch (currPrecedence)
+//                    {
+//                        case 3:
+//                            precedence3 += c;
+//                            break;
+//                        case 2:
+//                            precedence2 += c;
+//                            break;
+//                        case 1:
+//                            precedence1 += c;
+//                            break;
+//                    }
+//}
+//            }
+
+//            //Places operators into a string based on highest precedence first
+//            operators += precedence3 + precedence2 + precedence1;
+
+//            //Pushes operators onto stack in order of precedence
+//            foreach (char o in operators)
+//            {
+//                stack.Push(o);
+//            }
+
+//            foreach (char item in stack)
+//            {
+//                Console.WriteLine(item);
+//            }
+    }
+}
---
+++ StackApplicationBrady/Program.cs
@@ -0,0 +1,56 @@
+﻿/**************************************************************
+* Name        : StackApplicationBrady
+* Author      : Kabrina Brady
+* Created     : 2/9/2021
+* Course      : Data Structures
+* Version     : 1.0
+* OS          : Windows XX
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : This program overall description here
+*               Input:  list and describe
+*               Output: list and describe
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access to
+* my program.         
+***************************************************************/
+
+
+using System;
+using System.Collections;
+
+namespace StackApplicationBrady
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prints solutions
+            Console.WriteLine("Infix: 2 + 3 * 4");
+            Console.WriteLine("Postfix: 234 * +\n");
+
+            Console.WriteLine("Infix: a * b + 5");
+            Console.WriteLine("Postfix: ab * 5 +\n");
+
+            Console.WriteLine("Infix: (1 + 2) * 7");
+            Console.WriteLine("Postfix: 12 + 7 *\n");
+
+            Console.WriteLine("Infix: a * b / c");
+            Console.WriteLine("Postfix: ab * c /\n");
+
+            Console.WriteLine("Infix: (a / (b - c + d)) * (e - a) * c");
+            Console.WriteLine("Postfix: abc - d +/ ea - *c *\n");
+
+            Console.WriteLine("Infix: a / b - c + d * e - a * c");
+            Console.WriteLine("Postfix: ab / c - de * +ac * +-\n");
+
+            Console.WriteLine("Enter infix notation with no spaces:");
+            string infix = Console.ReadLine();
+
+            Converter c = new Converter();
+            string postfix = c.Convert(infix);
+            Console.WriteLine(postfix);
+        }
+    }
+}
