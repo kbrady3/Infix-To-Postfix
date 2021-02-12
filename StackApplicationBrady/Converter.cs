c4a2a7632956e5cf54dd9b3060ecb0633697c4e1
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-02-12 14:52:11 -0600

    Almost working

--- StackApplicationBrady/Converter.cs
+++ StackApplicationBrady/Converter.cs
@@ -10,63 +10,82 @@ namespace StackApplicationBrady
         public string Convert(string infix)
         {
             string output = "";
-            Console.WriteLine(infix);
             Stack stack = new Stack();
+            string top = Top(stack);
 
             foreach (char c in infix)
             {
-                Console.WriteLine(c);
+                //Print operands as they arrive
                 if (Char.IsNumber(c) || Char.IsLetter(c))
                 {
                     output += c;
                 }
-
-                if (IsOperator(c) && StackEmpty(stack))
+                //If the stack is empty or contains a left parenthesis on top, push the incoming operator onto the stack. 
+                else if (top == "(" || StackEmpty(stack))
                 {
                     stack.Push(c);
                 }
-                else if (IsOperator(c) && !StackEmpty(stack))
+                //If the incoming symbol is a right parenthesis, pop the stack and print the operators until you see a left parenthesis. Discard the pair of parentheses. 
+                else if (c == ')')
                 {
-                    string top = Top(stack);
-                    int precedenceOfTop = Precedence(top);
-                    int precedenceOfCurrent = Precedence(c.ToString());
-
-                    if (precedenceOfCurrent > precedenceOfTop)
+                    while(stack.Count > 0 && top != ")")
                     {
-                        stack.Push(c);
+                        output += stack.Peek();
+                        stack.Pop();
                     }
-     
-                    if (precedenceOfCurrent <= precedenceOfTop)
-                    {
-                        if (stack.Count > 0)
-                        {
-                            while(top != "(" && top != ")")
-                            {
-                                if(stack.Count > 0)
-                                {
-                                    stack.Pop();
-                                }
-                            }
-                        }
+                }
+                //If the incoming symbol has higher precedence than the top of the stack, push it on the stack. 
+                else if (Precedence(c.ToString()) > Precedence(top))
+                {
+                    stack.Push(c);
+                }
+                //If the incoming symbol has equal precedence with the top of the stack, use association. If the association is left to right, pop and print the top of the stack and then push the incoming operator. If the association is right to left, push the incoming operator. 
+                else if(Precedence(c.ToString()) == Precedence(top))
+                {
+                    output += stack.Peek();
+                    stack.Pop();
+                    stack.Push(c);
+                }
+                //If the incoming symbol has lower precedence than the symbol on the top of the stack, pop the stack and print the top operator. Then test the incoming operator against the new top of stack. 
+                else if (Precedence(c.ToString()) < Precedence(top))
+                {
+                    output += stack.Peek();
+                    stack.Pop();
 
-                        if (c == '(')
-                        {
-                            stack.Push(c);
-                        }
-                        else if (c == ')')
-                        {
-                            while (top != "(")
-                            {
-                                stack.Pop();
-                            }
-                        }
+                    if(Precedence(c.ToString()) < Precedence(top))
+                    {
+                        output += stack.Peek();
+                        stack.Pop();
                     }
                 }
             }
 
-            while (!StackEmpty(stack))
+            Stack tempStack = new Stack();
+
+            //At the end of the expression, pop and print all operators on the stack. (No parentheses should remain.)
+            while(stack.Count > 0)
             {
-                output += Top(stack);
+                tempStack.Push(stack.Peek());
+                stack.Pop();
+            }
+
+            //Reverses the order of the stack (so top is on bottom)
+            while (tempStack.Count > 0)
+            {
+                output += tempStack.Peek();
+                tempStack.Pop();
+            }
+
+            //Discard parentheses
+            if (output.Contains("("))
+            {
+                int index = output.IndexOf("(");
+                output = output.Remove(index, 1);
+            }
+            if (output.Contains(")"))
+            {
+                int index = output.IndexOf(")");
+                output = output.Remove(index, 1);
             }
 
             return output;
